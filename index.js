082354bbd689a603e857edb3ec1c8c558c097801
adambecker <adambeckercodes@gmail.com>
2023-12-14 17:36:47 +0600

    firts commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Basic function declaration
+// function printMe() {
+//   console.log("print me, bitch");
+// }
+
+function printThis(param) {
+  console.log(param);
+}
+
+printThis("blogger");
+
+// Function expression
+const count = 100; // An expression
+
+const printMe = function () {
+  console.log("print me bitch");
+};
